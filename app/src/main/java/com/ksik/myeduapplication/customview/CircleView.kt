dec42089b8a71e4240ad86cca837f1f1c5a12272
ksik <d.ksik94@gmail.com>
2022-04-25 23:03:20 +0900

    커스텀 뷰 edu

--- app/src/main/java/com/ksik/myeduapplication/customview/CircleView.kt
+++ app/src/main/java/com/ksik/myeduapplication/customview/CircleView.kt
@@ -5,23 +5,32 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.RelativeSizeSpan
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import com.ksik.myeduapplication.R
+import java.nio.charset.Charset
 
-class PieChart
+class CircleView
 @SuppressLint("Recycle") constructor(context: Context?, attrs: AttributeSet?) :
     View(context, attrs) { // 커스텀 속성을 참조하기 위한 변수
     private var myShapeColor: Int? = null // 생성자1: 코드에서 View 객체를 생성할 때 주로 호출
+    private var myText: String? = null // 생성자2: 코드에서 View 객체를 생성할 때 주로 호출
+    private var myTextColor: Int? = null // 생성자3: 코드에서 View 객체를 생성할 때 주로 호출
+    private var myTextSize: Float? = null // 생성자3: 코드에서 View 객체를 생성할 때 주로 호출
 
     init {
         if (attrs != null && context != null) {
-// attr.xml파일 declare-styleable이 CustomView로 정의된 attr(속성)을 typeArray객체로 받아온다.
-            val typedArr = context.obtainStyledAttributes(attrs, R.styleable.PieChart)
-// format을 구분하여 속성값 참조
-            myShapeColor = typedArr.getColor(R.styleable.PieChart_myShapeColor, Color.YELLOW)
-
+            // attr.xml파일 declare-styleable이 CustomView로 정의된 attr(속성)을 typeArray객체로 받아온다.
+            val typedArr = context.obtainStyledAttributes(attrs, R.styleable.CircleView)
+            // format을 구분하여 속성값 참조
+            myShapeColor = typedArr.getColor(R.styleable.CircleView_myShapeColor, Color.YELLOW)
+            myTextColor = typedArr.getColor(R.styleable.CircleView_myTextColor, Color.BLACK)
+            myText = typedArr.getString(R.styleable.CircleView_myTextString)
+            myTextSize = typedArr.getDimension(R.styleable.CircleView_myTextSize, 10f)
             typedArr.recycle()
         }
     }
@@ -29,7 +38,6 @@ class PieChart
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val width = MeasureSpec.getSize(widthMeasureSpec)
         val height = MeasureSpec.getSize(heightMeasureSpec)
-        Log.d("ksik", "onMeasure >> width : $width // height : $height")
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     }
 
@@ -41,22 +49,25 @@ class PieChart
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         val paint = Paint()
-// attr.xml 에서 참조해온 속성값을 적용
-        paint.color = myShapeColor ?: Color.BLACK
+        // attr.xml 에서 참조해온 속성값을 적용
         val width = width
         val height = height
-        Log.d("ksik", "onDraw >> width : $width // height : $height")
         var cx = width / 10.toFloat()
         var cy = height / 10.toFloat()
         val radi = 50f
         for (i in 0..4) {
+            paint.color = myShapeColor ?: Color.BLACK
             for (j in 0..1) {
                 canvas?.drawCircle(cx, cy, radi, paint)
                 cy += height / 14
 
+                paint.color = myTextColor ?: Color.BLACK
+                paint.textSize = myTextSize ?: 10f
+                myText?.let { canvas?.drawText(it, cx + 300, cy, paint) }
             }
             canvas?.drawCircle(cx, cy, radi, paint)
             cx += width / 5
         }
+
     }
 }
